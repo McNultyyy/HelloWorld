1c5ffba7d75fe56c5f55c26e467b95cad334b789
William McNulty <william.mcnulty@live.co.uk>
2018-12-25 13:03:57 +0000

    travis still

--- HelloWorld.Infrastructure/main.tf
+++ HelloWorld.Infrastructure/main.tf
@@ -6,22 +6,19 @@ provider "azurerm" {
 variable "applicationname" {
     default = "helloworld"
 }
-variable "resourcegroup" {
-    default = "hw-rs-01"
-}
 variable "location" {
     default = "uksouth"
 }
 
 
 resource "azurerm_resource_group" "resourcegroup" {
-  name     = "${var.resourcegroup}"
+  name     = "hwresourcegroup"
   location = "${var.location}"
 }
 
 resource "azurerm_storage_account" "storageaccount" {
-  name                     = "storageaccountname"
-  resource_group_name      = "${azurerm_resource_group.testrg.name}"
+  name                     = "hwstorage"
+  resource_group_name      = "${azurerm_resource_group.resourcegroup.name}"
   location                 = "${var.location}"
   account_tier             = "Standard"
   account_replication_type = "GRS"
