c29997e855e38f01e58e4449c02f1da1a33d4e6a
William McNulty <william.mcnulty@live.co.uk>
2018-12-30 14:13:56 +0000

    storage

--- HelloWorld.Infrastructure/main.tf
+++ HelloWorld.Infrastructure/main.tf
@@ -29,7 +29,8 @@ resource "azurerm_storage_account" "storageaccount" {
   resource_group_name      = "${azurerm_resource_group.resourcegroup.name}"
   location                 = "${var.location}"
   account_tier             = "Standard"
-  account_replication_type = "GRS"
+  account_replication_type = "GRS",
+  account_kind             = "StorageV2"
 
   tags {
     application = "${var.applicationname}"
