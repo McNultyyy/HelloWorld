7eb9f3caafbcf41d4de62a942a8bfcde53e7ead6
William McNulty <william.mcnulty@live.co.uk>
2018-12-30 00:30:19 +0000

    tfstate

--- HelloWorld.Infrastructure/main.tf
+++ HelloWorld.Infrastructure/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "azurerm" {
+    storage_account_name = "terraformaccount"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
 provider "azurerm" {
     version = "=1.20.00"
 }
